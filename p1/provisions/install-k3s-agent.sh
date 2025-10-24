923e7e13835769c7706c00e10e27707ddebf813d
lseiller <lseiller@student.42tokyo.jp>
2025-10-25 00:26:19 +0900

    Fix p1: remove argument that prevent the agent to connect to the server.

--- p1/provisions/install-k3s-agent.sh
+++ p1/provisions/install-k3s-agent.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export K3S_TOKEN_FILE="/vagrant/token"
 export K3S_URL="https://192.168.56.110:6443"
-curl -sfL https://get.k3s.io | sh -s - agent --write-kubeconfig-mode=644
+curl -sfL https://get.k3s.io | sh -s - agent #--write-kubeconfig-mode=644
