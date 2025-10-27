496ed32ac21874710fdd3e1a07fcf51998feecae
er1t <github42.gangrene880@passmail.net>
2025-10-27 16:49:00 +0100

    Working Part 3

--- p3/setup/argocd.sh
+++ p3/setup/argocd.sh
@@ -1,5 +1,5 @@
 # Create k3d cluster
-k3d cluster create -p "8080:443@loadbalancer" iot-part-3
+k3d cluster create iot-part-3
 
 # Wait for cluster to be available
 until kubectl version &> /dev/null; do
@@ -8,7 +8,12 @@ done
 
 # Setup argo CD
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml -f argocd-ingress.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml argocd-application.yaml
+
+until PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' 2> /dev/null); do
+    echo "waiting for password"
+	sleep 5
+done
 
 # Output password (very much secure (changeme))
-echo "Password: " $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d)
+echo "Password: " $(echo -n $PASSWORD | base64 -d)
