7d709c124dadabb4a91bada8521facb7049bba19
changeme <changeme@debi.debi>
2025-10-30 12:27:36 +0100

    Fix script

--- p3/setup/argocd.sh
+++ p3/setup/argocd.sh
@@ -8,12 +8,13 @@ done
 
 # Setup argo CD
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml argocd-application.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 until PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' 2> /dev/null); do
     echo "waiting for password"
 	sleep 5
 done
 
+kubectl apply -n argocd -f argocd-application.yaml
 # Output password (very much secure (changeme))
 echo "Password: " $(echo -n $PASSWORD | base64 -d)
