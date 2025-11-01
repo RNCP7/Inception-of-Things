77a2ba79d13381b7433493a765a895a5d62bd859
changeme <changeme@debi.debi>
2025-11-01 17:40:02 +0100

    Working part 3 :3

--- p3/setup/argocd.sh
+++ p3/setup/argocd.sh
@@ -2,11 +2,18 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+# Waiting for password
 until PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' 2> /dev/null); do
     echo "waiting for password"
 	sleep 5
 done
 
-kubectl apply -n argocd -f argocd-application.yaml -f ingress.yaml
+# Reconfigure cmd-params config map
+kubectl apply -f argocd-cmd-params-cm.yaml
+kubectl delete -n argocd $(kubectl get po -n argocd --selector=app.kubernetes.io/name=argocd-server -o name)
+
+kubectl apply -n argocd -f argocd-ingress.yaml
+
+kubectl apply -n argocd -f argocd-application.yaml
 # Output password (very much secure (changeme))
 echo "Password: " $(echo -n $PASSWORD | base64 -d)
--- p3/setup/cluster.sh
+++ p3/setup/cluster.sh
@@ -1,9 +1,10 @@
-# Create k3d cluster
-k3d cluster create iot-part-3 -p "443:443@loadbalancer"
+# Create k3d clustera
+k3d cluster create iot-part-3 -p "443:443@loadbalancer" --wait --k3s-arg "--disable=traefik@server:0"
 
-# Wait for cluster to be available
-until kubectl version &> /dev/null; do
-	sleep 5
-done
+# Setting up traefik
+helm repo add traefik https://traefik.github.io/charts
+helm install traefik traefik/traefik -n kube-system -f traefik-helm-chart-values.yaml --wait
+#kubectl apply -f traefik-helm-chart-values.yaml
 
+# Exec argocd script
 exec ./argocd.sh
